36e8d70e8827f9d68b72e681ed2e52e38ebc8b09
Prats Alban <aprats@TC405-109-10.insa-lyon.fr>
2020-01-06 10:53:57 +0100

    in progress

--- db-client.js
+++ db-client.js
@@ -1,4 +1,18 @@
-const PORT = 3000;
+const path = require("path");
+
+const getConfig = function getConfig() {
+  if (process.argv.length < 3) {
+    console.error("Vous devez indiquer le fichier de configuration");
+    console.info(`Usage : node ${process.argv[1]} <fileName>`);
+    throw new Error("Need config filename");
+  } else {
+    return require(path.resolve(process.argv[2]));
+  }
+}
+
+const config = getConfig();
+
+const PORT = config.port;
 
 const io = require('socket.io-client');
 
