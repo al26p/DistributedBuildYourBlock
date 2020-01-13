3513a787ddbd4faa9894cb709fced9f6d33b5bdf
Prats Alban <aprats@TC405-112-11.insa-lyon.fr>
2020-01-13 10:59:17 +0100

    fin etape 2

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const process = require("process");
 const getConfig = function getConfig() {
   if (process.argv.length < 3) {
     console.error("Vous devez indiquer le fichier de configuration");
@@ -11,38 +11,75 @@ const getConfig = function getConfig() {
 }
 
 const config = getConfig();
-
 const PORT = config.port;
-
 const Server = require('socket.io');
+var ff = config.friends;
+const regex = /::ffff:/;
+
 const io = new Server(PORT, {
   path: '/dbyb',
   serveClient: false,
 });
 
+const addPeer = (f) => {
+  const s = io_cli(f, {
+    path: '/dbyb',
+  });
+  friends.push(s);
+};
+
 console.log(`Serveur lancé sur le port ${PORT}.`);
 
 const io_cli = require('socket.io-client');
 var friends = new Array(0);
-for (const p of config.friends){
-  const s = io_cli(`http://localhost:${p}`, {
-    path: '/dbyb',
-  });
-  friends.push(s);
+for (const f of ff){
+  addPeer(f)
 }
-
 console.log(`Serveurs connectés`);
 
 const db = Object.create(null);
 
+const check = () => {
+  for(const f of friends){
+    f.emit('getPeers', '', (peers) => {
+      for (const f of peers) {
+        console.log("add", f)
+        addPeer(f)
+      }
+
+      f.emit('register', config.port, (cb) => {
+        console.log('recieved', cb)
+        if(!cb){
+          console.log('Failed to register');
+          process.exit();
+        }
+      });
+    });
+    f.emit('keys', '', value => {
+      for(const c of value){
+        if(!(c in db)){
+        f.emit('get', c, (v) => {
+            db[c] = v;
+            console.log('added new value');
+          });
+        }
+      }
+    });
+  }
+};
+
+check();
+
 io.on('connect', (socket) => {
   console.log('Nouvelle connexion');
 
+  //Get values
   socket.on('get', function(field, callback){
     console.log(`get ${field}: ${db[field]}`);
     callback(db[field]);
   });
 
+  //Set new value
   socket.on('set', function(field, value, callback){
     if (field in db) {
       console.log(`set error : Field ${field} exists.`);
@@ -60,8 +97,22 @@ io.on('connect', (socket) => {
     }
   });
 
+  //Get keys from db
   socket.on('keys', function(field, callback){
     let keys = Object.keys(db);
     callback(keys);
+  });
+
+  //Register new server
+  socket.on('register', function(pport, callback){
+    let add = "" + socket.handshake.address;
+    add = add.replace(regex, '')
+    addPeer('http://' + add + ':' + pport);
+    ff.push('http://' + add + ':' + pport);
+    callback(true);
+  });
+
+  socket.on('getPeers', function(field, callback){
+    callback(ff);
   });
 });
